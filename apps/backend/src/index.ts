4f7843b5a999c458f3f0c63ea33e6e7cf49a41a3
AYUSH SINGH <ayushsinghpanchet07@gmail.com>
2025-09-29 22:00:20 +0530

    clean up

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -5,7 +5,6 @@ import path from 'path';
 import dotenv from 'dotenv';
 import { FalAiModel } from './model/FalAiModel';
 import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
-import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import cors from 'cors';
 
 
